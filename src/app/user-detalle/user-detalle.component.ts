1f79515965e131191e04ce7317b06e651e3c7f6b
FedericoUTN <hulalaoooo@gmail.com>
2021-07-08 20:41:30 -0300

    update edit

--- src/app/user-detalle/user-detalle.component.ts
+++ src/app/user-detalle/user-detalle.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '../user/User';
 import { UserService } from '../user/user.service';
@@ -11,13 +12,26 @@ import { UserService } from '../user/user.service';
 export class UserDetalleComponent implements OnInit {
 user: User = {login: '', avatar_url: '', id: 0, node_id: '', gravatar_id: '', name: '', location: '', html_url: ''};
 login = '';
-  constructor(private route: ActivatedRoute, private userService: UserService) {
-
+isEdit: boolean = false;
+formUser: FormGroup;
+  constructor(private route: ActivatedRoute, private userService: UserService, private formBuilder: FormBuilder) {
+    this.formUser = this.formBuilder.group({
+      nombre: [''],
+      localidad: [''],
+      git: ['']
+    })
    }
 
   ngOnInit(): void {
     this.route.params.subscribe( params => this.login = params.login);
     this.userService.getById(this.login).subscribe( (data: User) => this.user = data)
   }
+  editOn(): void{
+    this.isEdit = !this.isEdit;
+  }
+  save(): void{
+    
+  }
+  
 
 }
